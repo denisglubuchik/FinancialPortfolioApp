f0d1583489c745e01e45d13bf0e6c80dbd435b2e
glubuchikd <glubuchikdenis@gmail.com>
2024-10-16 11:40:16 +0300

    assets router updated

--- back/portfolio_service/routers/assets.py
+++ back/portfolio_service/routers/assets.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from back.portfolio_service.dependencies import UOWDep
 from back.portfolio_service.exceptions import AssetAlreadyExistException, AssetDoesntExistException
-from back.portfolio_service.schemas.assets import SAsset, SAssetCreate
+from back.portfolio_service.schemas.assets import SAsset, SAssetCreate, AssetType
 from back.portfolio_service.services.assets import AssetsService
 
 router = APIRouter(
@@ -12,22 +12,23 @@ router = APIRouter(
 
 
 @router.post("/")
-async def add_asset(asset: SAssetCreate, uow: UOWDep):
-    asset = await AssetsService().get_asset(uow, asset.id)
+async def add_asset(new_asset: SAssetCreate, uow: UOWDep):
+    asset = await AssetsService().get_asset(uow, name=new_asset.name)
     if asset:
         raise AssetAlreadyExistException()
-    await AssetsService().add_asset(uow, asset)
+    asset_id = await AssetsService().add_asset(uow, new_asset)
+    return asset_id
 
 
 @router.get("/")
-async def get_assets(uow: UOWDep) -> list[SAsset]:
-    assets = await AssetsService().get_all_assets(uow)
+async def get_assets(uow: UOWDep, asset_type: AssetType = None) -> list[SAsset]:
+    assets = await AssetsService().get_all_assets(uow, asset_type)
     return assets
 
 
 @router.get("/{asset_id}")
 async def get_asset(asset_id: int, uow: UOWDep) -> SAsset:
-    asset = await AssetsService().get_asset(uow, asset_id)
+    asset = await AssetsService().get_asset_by_id(uow, asset_id)
     if not asset:
         raise AssetDoesntExistException()
     return asset
@@ -35,4 +36,7 @@ async def get_asset(asset_id: int, uow: UOWDep) -> SAsset:
 
 @router.delete("/{asset_id}")
 async def delete_asset(asset_id: int, uow: UOWDep):
+    asset = await AssetsService().get_asset_by_id(uow, asset_id)
+    if not asset:
+        raise AssetDoesntExistException()
     await AssetsService().delete_asset(uow, asset_id)
--- back/portfolio_service/services/assets.py
+++ back/portfolio_service/services/assets.py
@@ -5,17 +5,26 @@ from back.portfolio_service.utils.uow import IUnitOfWork
 class AssetsService:
     async def add_asset(self, uow: IUnitOfWork, asset: SAssetCreate):
         async with uow:
-            await uow.assets.add(asset.model_dump())
+            asset_id = await uow.assets.add(asset.model_dump())
             await uow.commit()
+            return asset_id
 
-    async def get_asset(self, uow: IUnitOfWork, asset_id: int):
+    async def get_asset_by_id(self, uow: IUnitOfWork, asset_id: int):
         async with uow:
             asset = await uow.assets.get_one(id=asset_id)
             return asset
 
-    async def get_all_assets(self, uow: IUnitOfWork):
+    async def get_asset(self, uow, **kwargs):
         async with uow:
-            assets = await uow.assets.get_all()
+            asset = await uow.assets.get_one(**kwargs)
+            return asset
+
+    async def get_all_assets(self, uow: IUnitOfWork, asset_type):
+        async with uow:
+            if asset_type:
+                assets = await uow.assets.get_all(asset_type=asset_type)
+            else:
+                assets = await uow.assets.get_all()
             return assets
 
     async def delete_asset(self, uow: IUnitOfWork, asset_id: int):
